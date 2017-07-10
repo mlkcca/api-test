16cdbd9165eaa410413c96e5750f813caca765c3
Shuhei Hiya <syuhei176@gmail.com>
2017-07-10 19:49:20 +0900

    send by mqtt

--- test/mqtt/test.js
+++ test/mqtt/test.js
@@ -11,7 +11,7 @@ describe("mqtt", function() {
 
     it('push message', function (done) {
 
-      var topic = 'demo/apitest-mqtt/push'
+      var topic = 'demo/apitest-mqtt/_p'
 
       var client  = mqtt.connect('mqtt://' + mlkccaEndpoint, {
         username: 'kdemo',
@@ -37,4 +37,35 @@ describe("mqtt", function() {
 
   })
 
+  describe("send", function() {
+
+    this.timeout(5000)
+
+    it('send message', function (done) {
+
+      var topic = 'demo/apitest-mqtt/_s'
+
+      var client  = mqtt.connect('mqtt://' + mlkccaEndpoint, {
+        username: 'kdemo',
+        password: 'demo'
+      })
+      client.on('connect', function (e) {
+        client.subscribe(topic)
+        setTimeout(function() {
+          client.publish(topic, JSON.stringify({content:"Hello"}))
+        }, 100)
+      })
+       
+      client.on('message', function (topic, message) {
+        console.log("message", topic, result)
+        var str = message.toString();
+        var result = JSON.parse(str);
+        assert.equal(result.content, "Hello");
+        client.end()
+        done()
+      })
+
+    });
+
+  })
 })
