903942c32e85065e9e360da8aa6b90874f91c4fc
Kiyohiko Heima <piyo326@gmail.com>
2017-11-29 16:35:14 +0900

    Update http test

--- test/http/api_test.js
+++ test/http/api_test.js
@@ -5,12 +5,14 @@ const mlkccaEndpoint = 'https://pubsub1.mlkcca.com'
 const pushURL = '/api/push/demo/demo?c=apitest'
 const onPushURL = '/on/push/demo/demo?c=[["apitest",0]]'
 const historyURL = '/api/history/demo/demo?c=apitest'
+const dsURL = '/api/ds/demo/demo?c=apitest'
+const grantURL = '/api/grant/demo/demo'
 
 describe("http", function() {
 
   describe("push", function() {
 
-    var agent = request.agent(mlkccaEndpoint) 
+    var agent = request.agent(mlkccaEndpoint)
 
     it('push message', function (done) {
       agent
@@ -35,7 +37,7 @@ describe("http", function() {
 
   describe("history", function() {
 
-    var agent = request.agent(mlkccaEndpoint) 
+    var agent = request.agent(mlkccaEndpoint)
 
     it('get history', function (done) {
       agent
@@ -55,14 +57,14 @@ describe("http", function() {
       });
 
     });
-    it('get history previous 1499680910228', function (done) {
+    it('get history previous 1499682206324', function (done) {
       agent
-      .get(historyURL + '&limit=5&ts=1499680910228')
+      .get(historyURL + '&limit=5&ts=1499682206324')
       //.set('Accept', 'application/json')
       .expect(function(res) {
 
         let result = JSON.parse(res.text)
-        assert.equal(result.content[0].t, 1499680909693);
+        assert.equal(result.content[0].t, 1499682205473);
 
       }).end(function(err,res) {
         if(err){
@@ -76,9 +78,60 @@ describe("http", function() {
 
   })
 
+  describe("ds", function() {
+
+    var agent = request.agent(mlkccaEndpoint)
+
+    it('get apitest datastore', function (done) {
+      agent
+      .get(dsURL)
+      .expect(function(res) {
+
+        let result = JSON.parse(res.text)
+        assert.equal(result.content[0], 'apitest');
+
+      }).end(function(err,res) {
+        if(err){
+            throw err;
+        }
+        done();
+      });
+
+    });
+
+  })
+
+  describe("grant", function() {
+
+    var agent = request.agent(mlkccaEndpoint)
+
+    it('grant push API to apitest-grant', function (done) {
+      agent
+      .post(grantURL)
+      .send({"rules":{"apitest-grant":["push"]},"ttl":3600})
+      .expect(function(res) {
+
+        let result = JSON.parse(res.text)
+        assert.equal(result.content.rules['apitest-grant'][0], 'push');
+        assert.equal(result.content.ttl, 3600);
+
+      }).end(function(err,res) {
+        if(err){
+            throw err;
+        }
+        done();
+      });
+
+    });
+
+  })
+
+
   describe("on", function() {
 
-    var agent = request.agent(mlkccaEndpoint) 
+    this.timeout(10000)
+
+    var agent = request.agent(mlkccaEndpoint)
 
     it('subscribe message', function (done) {
       agent
