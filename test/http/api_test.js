1209e32144ad9adeffc01efe6a857f21f8a3a1e8
Kiyohiko Heima <piyo326@gmail.com>
2017-11-30 03:53:16 +0900

    Update HTTP /push/

--- test/http/api_test.js
+++ test/http/api_test.js
@@ -1,5 +1,8 @@
-var request = require('supertest');
-var assert = require('assert');
+var request = require('supertest')
+var assert = require('assert')
+
+const uuidv4 = require('uuid/v4')
+global.uuid = uuidv4()
 
 const mlkccaEndpoint = 'https://pubsub1.mlkcca.com'
 const pushURL = '/api/push/demo/demo?c=apitest'
@@ -8,127 +11,80 @@ const historyURL = '/api/history/demo/demo?c=apitest'
 const dsURL = '/api/ds/demo/demo?c=apitest'
 const grantURL = '/api/grant/demo/demo'
 
-describe("http", function() {
-
-  describe("push", function() {
-
-    var agent = request.agent(mlkccaEndpoint)
-
-    it('push message', function (done) {
-      agent
-      .get(pushURL+'&v={"val":10}')
-      //.set('Accept', 'application/json')
-      .expect(function(res) {
-
-        let result = JSON.parse(res.text)
-        assert.equal(res.status, 200);
-        assert.equal(result.err, null);
-
-      }).end(function(err,res) {
-        if(err){
-            throw err;
-        }
-        done();
-      });
-
-    });
-
-  })
-
-  describe("history", function() {
-
+describe('HTTP', function () {
+  describe('history', function () {
     var agent = request.agent(mlkccaEndpoint)
 
     it('get history', function (done) {
       agent
       .get(historyURL)
-      //.set('Accept', 'application/json')
-      .expect(function(res) {
-
+      // .set('Accept', 'application/json')
+      .expect(function (res) {
         let result = JSON.parse(res.text)
-        assert.equal(res.status, 200);
-        assert.equal(result.err, null);
-
-      }).end(function(err,res) {
-        if(err){
-            throw err;
+        assert.equal(res.status, 200)
+        assert.equal(result.err, null)
+      }).end(function (err, res) {
+        if (err) {
+          throw err
         }
-        done();
-      });
-
-    });
+        done()
+      })
+    })
     it('get history previous 1499682206324', function (done) {
       agent
       .get(historyURL + '&limit=5&ts=1499682206324')
-      //.set('Accept', 'application/json')
-      .expect(function(res) {
-
+      // .set('Accept', 'application/json')
+      .expect(function (res) {
         let result = JSON.parse(res.text)
-        assert.equal(result.content[0].t, 1499682205473);
-
-      }).end(function(err,res) {
-        if(err){
-            throw err;
+        assert.equal(result.content[0].t, 1499682205473)
+      }).end(function (err, res) {
+        if (err) {
+          throw err
         }
-        done();
-      });
-
-    });
-
-
+        done()
+      })
+    })
   })
 
-  describe("ds", function() {
-
+  describe('ds', function () {
     var agent = request.agent(mlkccaEndpoint)
 
     it('get apitest datastore', function (done) {
       agent
       .get(dsURL)
-      .expect(function(res) {
-
+      .expect(function (res) {
         let result = JSON.parse(res.text)
-        assert.equal(result.content[0], 'apitest');
-
-      }).end(function(err,res) {
-        if(err){
-            throw err;
+        assert.equal(result.content[0], 'apitest')
+      }).end(function (err, res) {
+        if (err) {
+          throw err
         }
-        done();
-      });
-
-    });
-
+        done()
+      })
+    })
   })
 
-  describe("grant", function() {
-
+  describe('grant', function () {
     var agent = request.agent(mlkccaEndpoint)
 
     it('grant push API to apitest-grant', function (done) {
       agent
       .post(grantURL)
-      .send({"rules":{"apitest-grant":["push"]},"ttl":3600})
-      .expect(function(res) {
-
+      .send({'rules': {'apitest-grant': ['push']}, 'ttl': 3600})
+      .expect(function (res) {
         let result = JSON.parse(res.text)
-        assert.equal(result.content.rules['apitest-grant'][0], 'push');
-        assert.equal(result.content.ttl, 3600);
-
-      }).end(function(err,res) {
-        if(err){
-            throw err;
+        assert.equal(result.content.rules['apitest-grant'][0], 'push')
+        assert.equal(result.content.ttl, 3600)
+      }).end(function (err, res) {
+        if (err) {
+          throw err
         }
-        done();
-      });
-
-    });
-
+        done()
+      })
+    })
   })
 
-
-  describe("on", function() {
-
+  describe('on', function () {
     this.timeout(10000)
 
     var agent = request.agent(mlkccaEndpoint)
@@ -136,39 +92,29 @@ describe("http", function() {
     it('subscribe message', function (done) {
       agent
       .get(onPushURL)
-      .expect(function(res) {
-
+      .expect(function (res) {
         let result = JSON.parse(res.text)
         let value = result.apitest[0][2]
-        assert.equal(res.status, 200);
-        assert.equal(value, '{"val":"Hello"}');
-
-      }).end(function(err,res) {
-        if(err){
-            throw err;
+        assert.equal(res.status, 200)
+        assert.equal(value, '{"val":"Hello"}')
+      }).end(function (err, res) {
+        if (err) {
+          throw err
         }
-        done();
-      });
+        done()
+      })
 
       agent
-      .get(pushURL+'&v={"val":"Hello"}')
-      .expect(function(res) {
-
+      .get(pushURL + '&v={"val":"Hello"}')
+      .expect(function (res) {
         let result = JSON.parse(res.text)
-        assert.equal(res.status, 200);
-        assert.equal(result.err, null);
-
-      }).end(function(err,res) {
-        if(err){
-            throw err;
+        assert.equal(res.status, 200)
+        assert.equal(result.err, null)
+      }).end(function (err, res) {
+        if (err) {
+          throw err
         }
-
-      });
-
-
-    });
-
+      })
+    })
   })
-
-
 })
---
+++ test/http/push.js
@@ -0,0 +1,91 @@
+const request = require('supertest')
+const uuidv4 = require('uuid/v4')
+
+function Push (uuid) {
+  const mlkccaEndpoint = 'https://pubsub1.mlkcca.com'
+  const pushURL = '/api/push/demo/demo?c={{dspath}}'
+  // const onPushURL = '/on/push/demo/demo?c=[["{{dspath}}",0]]'
+
+  describe('GET /push/', function () {
+    let agent = request.agent(mlkccaEndpoint)
+
+    it('should return 500 when no paramaters', function (done) {
+      agent
+      .get(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/get'))
+      .expect(500)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 400 when v === empty', function (done) {
+      agent
+      .get(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/get&v='))
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 200 when paramaters are valid', function (done) {
+      agent
+      .get(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/get') + '&v={"val":10}')
+      .expect(function (res) {
+        let result = JSON.parse(res.text)
+        res.body = {
+          err: result.err
+        }
+      })
+      .expect(200, {
+        err: null
+      }, done)
+    })
+  })
+
+  describe('POST /push/', function () {
+    let agent = request.agent(mlkccaEndpoint)
+
+    it('should return 500 when no paramaters', function (done) {
+      agent
+      .post(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/post'))
+      .send({})
+      .expect(500)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 400 when v === empty', function (done) {
+      agent
+      .post(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/post'))
+      .send({v: ''})
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 200 when paramaters are valid', function (done) {
+      agent
+      .post(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/post'))
+      .send({v: '{"val":10}'})
+      .expect(function (res) {
+        let result = JSON.parse(res.text)
+        res.body = {
+          err: result.err
+        }
+      })
+      .expect(200, {
+        err: null
+      }, done)
+    })
+  })
+}
+
+Push(global.uuid || uuidv4())
+
+module.exports = Push
