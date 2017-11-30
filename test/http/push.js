522f8ad304e65483b2267ad30ef9c6a113767cd9
Kiyohiko Heima <piyo326@gmail.com>
2017-11-30 17:04:40 +0900

    Add all case of Push

--- test/http/push.js
+++ test/http/push.js
@@ -4,12 +4,42 @@ const uuidv4 = require('uuid/v4')
 function Push (uuid) {
   const mlkccaEndpoint = 'https://pubsub1.mlkcca.com'
   const pushURL = '/api/push/demo/demo?c={{dspath}}'
-  // const onPushURL = '/on/push/demo/demo?c=[["{{dspath}}",0]]'
+  const pushURLWrongAPIKey = '/api/push/demo/wrongapikey?c={{dspath}}'
 
   describe('GET /push/', function () {
     let agent = request.agent(mlkccaEndpoint)
 
-    it('should return 400 when no paramaters', function (done) {
+    it('should return 403 if apikey is wrong', function (done) {
+      agent
+      .get(pushURLWrongAPIKey.replace(/{{dspath}}/, 'http/' + uuid + '/push/get&v={"val":10}'))
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 403 if no datastorePath', function (done) {
+      agent
+      .get('/api/push/demo/demo?v=1')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 403 if datastorePath === empty', function (done) {
+      agent
+      .get('/api/push/demo/demo?c=&v=1')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 400 if no v param', function (done) {
       agent
       .get(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/get'))
       .expect(400)
@@ -19,7 +49,7 @@ function Push (uuid) {
       })
     })
 
-    it('should return 400 when v === empty', function (done) {
+    it('should return 400 if v === empty', function (done) {
       agent
       .get(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/get&v='))
       .expect(400)
@@ -47,7 +77,40 @@ function Push (uuid) {
   describe('POST /push/', function () {
     let agent = request.agent(mlkccaEndpoint)
 
-    it('should return 400 when no paramaters', function (done) {
+    it('should return 403 if apikey is wrong', function (done) {
+      agent
+      .post(pushURLWrongAPIKey.replace(/{{dspath}}/, 'http/' + uuid + '/push/post'))
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 403 if no datastorePath', function (done) {
+      agent
+      .post('/api/push/demo/demo')
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 403 if datastorePath === empty', function (done) {
+      agent
+      .post('/api/push/demo/demo?c=')
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('should return 400 if no v param', function (done) {
       agent
       .post(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/post'))
       .send({})
@@ -58,7 +121,7 @@ function Push (uuid) {
       })
     })
 
-    it('should return 400 when v === empty', function (done) {
+    it('should return 400 if v === empty', function (done) {
       agent
       .post(pushURL.replace(/{{dspath}}/, 'http/' + uuid + '/push/post'))
       .send({v: ''})
