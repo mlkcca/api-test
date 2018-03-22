b55e6685dad5db9b3d6b256d285d9e02f66fe92b
Kiyohiko Heima <piyo326@gmail.com>
2018-03-22 14:13:25 +0900

    Add http push&send&set error test

--- test/http/push.js
+++ test/http/push.js
@@ -5,61 +5,61 @@ const settings = require('../../settings')[process.env.NODE_ENV || 'production']
 function Push (uuid) {
   const mlkccaEndpoint = settings.endpoint
   const pushURL = '/api/push/' + settings.appId + '/' + settings.apiKey
-  // const pushURLWrongAPIKey = '/api/push/' + settings.appId + '/wrongapikey'
+  const pushURLWrongAPIKey = '/api/push/' + settings.appId + '/wrongapikey'
 
   describe('GET /push/', function () {
     this.timeout(5000)
     let agent = request.agent(mlkccaEndpoint)
 
-    // it('should return 403 if apikey is wrong', function (done) {
-    //   agent
-    //   .get(pushURLWrongAPIKey + '?c=http/' + uuid + '/push/get&v={"val":10}')
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if apikey is wrong', function (done) {
+      agent
+      .get(pushURLWrongAPIKey + '?c=http/' + uuid + '/push/get&v={"val":10}')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if no datastorePath', function (done) {
-    //   agent
-    //   .get(pushURL + '?v=1')
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if no datastorePath', function (done) {
+      agent
+      .get(pushURL + '?v=1')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if datastorePath === empty', function (done) {
-    //   agent
-    //   .get(pushURL + '?c=&v=1')
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if datastorePath === empty', function (done) {
+      agent
+      .get(pushURL + '?c=&v=1')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if no v param', function (done) {
-    //   agent
-    //   .get(pushURL + '?c=http/' + uuid + '/push/get')
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if no v param', function (done) {
+      agent
+      .get(pushURL + '?c=http/' + uuid + '/push/get')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if v === empty', function (done) {
-    //   agent
-    //   .get(pushURL + '?c=http/' + uuid + '/push/get&v=')
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if v === empty', function (done) {
+      agent
+      .get(pushURL + '?c=http/' + uuid + '/push/get&v=')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
     it('should return 200 when paramaters are valid', function (done) {
       agent
@@ -79,60 +79,60 @@ function Push (uuid) {
   describe('POST /push/', function () {
     let agent = request.agent(mlkccaEndpoint)
 
-    // it('should return 403 if apikey is wrong', function (done) {
-    //   agent
-    //   .post(pushURLWrongAPIKey + '?c=http/' + uuid + '/push/post')
-    //   .send({v: 2})
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if apikey is wrong', function (done) {
+      agent
+      .post(pushURLWrongAPIKey + '?c=http/' + uuid + '/push/post')
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if no datastorePath', function (done) {
-    //   agent
-    //   .post(pushURL)
-    //   .send({v: 2})
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if no datastorePath', function (done) {
+      agent
+      .post(pushURL)
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if datastorePath === empty', function (done) {
-    //   agent
-    //   .post(pushURL + '?c=')
-    //   .send({v: 2})
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if datastorePath === empty', function (done) {
+      agent
+      .post(pushURL + '?c=')
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if no v param', function (done) {
-    //   agent
-    //   .post(pushURL + '?c=http/' + uuid + '/push/post')
-    //   .send({})
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if no v param', function (done) {
+      agent
+      .post(pushURL + '?c=http/' + uuid + '/push/post')
+      .send({})
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if v === empty', function (done) {
-    //   agent
-    //   .post(pushURL + '?c=http/' + uuid + '/push/post')
-    //   .send({v: ''})
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if v === empty', function (done) {
+      agent
+      .post(pushURL + '?c=http/' + uuid + '/push/post')
+      .send({v: ''})
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
     it('should return 200 when paramaters are valid', function (done) {
       agent
--- test/http/set.js
+++ test/http/set.js
@@ -5,61 +5,61 @@ const settings = require('../../settings')[process.env.NODE_ENV || 'production']
 function Set (uuid) {
   const mlkccaEndpoint = settings.endpoint
   const setURL = '/api/set/' + settings.appId + '/' + settings.apiKey
-  // const setURLWrongAPIKey = '/api/set/' + settings.appId + '/wrongapikey'
+  const setURLWrongAPIKey = '/api/set/' + settings.appId + '/wrongapikey'
 
   describe('GET /set/', function () {
     this.timeout(5000)
     let agent = request.agent(mlkccaEndpoint)
 
-    // it('should return 403 if apikey is wrong', function (done) {
-    //   agent
-    //   .get(setURLWrongAPIKey + '?c=http/' + uuid + '/set/get&v={"val":10}')
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if apikey is wrong', function (done) {
+      agent
+      .get(setURLWrongAPIKey + '?c=http/' + uuid + '/set/get&v={"val":10}')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if no datastorePath', function (done) {
-    //   agent
-    //   .get(setURL + '?v=1')
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if no datastorePath', function (done) {
+      agent
+      .get(setURL + '?v=1')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if datastorePath === empty', function (done) {
-    //   agent
-    //   .get(setURL + '?c=&v=1')
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if datastorePath === empty', function (done) {
+      agent
+      .get(setURL + '?c=&v=1')
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if no v param', function (done) {
-    //   agent
-    //   .get(setURL + '?c=http/' + uuid + '/set/get')
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if no v param', function (done) {
+      agent
+      .get(setURL + '?c=http/' + uuid + '/set/get')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if v === empty', function (done) {
-    //   agent
-    //   .get(setURL + '?c=http/' + uuid + '/set/get&v=')
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if v === empty', function (done) {
+      agent
+      .get(setURL + '?c=http/' + uuid + '/set/get&v=')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
     it('should return 200 when paramaters are valid', function (done) {
       agent
@@ -79,60 +79,60 @@ function Set (uuid) {
   describe('POST /set/', function () {
     let agent = request.agent(mlkccaEndpoint)
 
-    // it('should return 403 if apikey is wrong', function (done) {
-    //   agent
-    //   .post(setURLWrongAPIKey + '?c=http/' + uuid + '/set/post')
-    //   .send({v: 2})
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if apikey is wrong', function (done) {
+      agent
+      .post(setURLWrongAPIKey + '?c=http/' + uuid + '/set/post')
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if no datastorePath', function (done) {
-    //   agent
-    //   .post(setURL)
-    //   .send({v: 2})
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if no datastorePath', function (done) {
+      agent
+      .post(setURL)
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 403 if datastorePath === empty', function (done) {
-    //   agent
-    //   .post(setURL + '?c=')
-    //   .send({v: 2})
-    //   .expect(403)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 403 if datastorePath === empty', function (done) {
+      agent
+      .post(setURL + '?c=')
+      .send({v: 2})
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if no v param', function (done) {
-    //   agent
-    //   .post(setURL + '?c=http/' + uuid + '/set/post')
-    //   .send({})
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if no v param', function (done) {
+      agent
+      .post(setURL + '?c=http/' + uuid + '/set/post')
+      .send({})
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
-    // it('should return 400 if v === empty', function (done) {
-    //   agent
-    //   .post(setURL + '?c=http/' + uuid + '/set/post')
-    //   .send({v: ''})
-    //   .expect(400)
-    //   .end(function (err, res) {
-    //     if (err) return done(err)
-    //     done()
-    //   })
-    // })
+    it('should return 400 if v === empty', function (done) {
+      agent
+      .post(setURL + '?c=http/' + uuid + '/set/post')
+      .send({v: ''})
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+    })
 
     it('should return 200 when paramaters are valid', function (done) {
       agent
